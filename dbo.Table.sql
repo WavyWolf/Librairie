94b53e75bc7b415e916ce9d280a468681be0dc5b
WavyWolf <thinhung.phan40@gmail.com>
2023-04-11 12:06:45 +0200

    rajout

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Livres]
+(
+	[LId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [LNom] NCHAR(150) NULL, 
+    [LAuteur] INT NOT NULL, 
+    [LCategorie] INT NOT NULL,
+    [LQuantite] INT NOT NULL, 
+    [LPrix] INT NOT NULL
+)
